cf185fa5e51e081ce4ec47a34ffa9ec5b66c6d45
dkz97 <r2cryptocn@gmail.com>
2022-10-07 19:21:10 +0800

    先丢三个功能按钮，后续实现

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+// 引入样式
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          进入主页
-        </p>
-        <button onClick={ handlerClick }>
-          Learn React
-        </button>
+        <div>
+        <Button className="buttonClass" type="primary" size='large' onClick={claimAll}>
+          一键领取
+        </Button>
+        <Button className="buttonClass" type="primary" size='large' onClick={distributeAll}>
+          一键发送
+        </Button>
+        <Button className="buttonClass" type="primary" size='large' onClick={stakeAll}>
+          一键质押
+        </Button>
+        </div>
       </header>
     </div>
   );
 }
 
-function handlerClick() {
-  console.log('click');
+// claim当前账户的奖励，先支持 atom, osmo, scrt, juno, evmos
+function claimAll() {
+  console.log("claimAll");
 }
+
+// 将当前账户的所有资产发送到指定地址，先支持 atom, osmo, scrt, juno, evmos
+function distributeAll() {
+  console.log("distributeAll");
+}
+
+// 质押当前账户的所有资产，先支持 atom, osmo, scrt, juno, evmos
+function stakeAll() {
+  console.log("stakeAll");
+}
+
 
 export default App;
