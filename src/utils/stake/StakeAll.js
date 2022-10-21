a2d34a396efa8fea29c93ca6f8e2b027a0f29b17
dkz97 <r2cryptocn@gmail.com>
2022-10-21 17:04:41 +0800

    stake

--- src/utils/stake/StakeAll.js
+++ src/utils/stake/StakeAll.js
@@ -5,7 +5,15 @@
 export default class StakeAll { 
  
     async stakeAll() {
-        
+        // 遍历循环chainslist，并且await distribute
+        for (let chain of chainsList) {
+            console.log(`stake ${chain.chainId} start`);
+            await this.stake(chain)
+        }
+    }
+
+    async stake() {
+
     }
 
 }
